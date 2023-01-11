45222380cbe94975be98cd96604bc009f7981a59
yedidyab <yedidyab@access4.wexac.weizmann.ac.il>
2023-01-11 18:11:59 +0200

    add vizualization

--- scripts/visualization.py
+++ scripts/visualization.py
@@ -1,14 +1,12 @@
 import napari
-import nd2
-import utils
 import numpy as np
 
-def show_cell(image_path, index):
+def show_cell(image_path):
     image = np.load(image_path)
     viewer = napari.Viewer()
-    viewer.add_image(image.phase_projection)
-    viewer.add_image(image.dapi, name='dapi')
-    viewer.add_image(image.ribo, name='dapi')
-    viewer.add_image(image.dapi, name='dapi')
-    viewer.add_labels(image.masks_outlines)
+    viewer.add_image(image[0, ...], name='phase proj')
+    viewer.add_image(image[1, ...], name='masks')
+    viewer.add_image(image[2, ...], name='dapi')
+    viewer.add_image(image[3, ...], name='ribo')
+    viewer.add_image(image[4, ...], name='wga')
     viewer.show(block=True)  # wait until viewer window closes
